37e6bbd4a35440e64d61908bda1375b195943fb4
qiuny <Qwer@1230>
2024-06-12 23:16:23 +0800

    feat:新增svg图标模块

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,34 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+<script setup lang="js">
+import { BoldIcon, ItalicIcon } from "@/assets/icons";
+import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <!-- <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" /> -->
-      <svg-icon icon="vue"></svg-icon>
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-  <vue3-major-editor></vue3-major-editor>
+    <div>
+        <a href="https://vitejs.dev" target="_blank">
+            <img src="/vite.svg" class="logo" alt="Vite logo" />
+        </a>
+        <!-- <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" /> -->
+        <BoldIcon></BoldIcon>
+        <ItalicIcon></ItalicIcon>
+    </div>
+    <HelloWorld msg="Vite + Vue" />
+    <vue3-major-editor></vue3-major-editor>
 </template>
 
 <style scoped>
 .logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
 }
+
 .logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+    filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+    filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
---
+++ src/assets/icons/index.js
@@ -0,0 +1,13 @@
+// const vueSvg = new URL('./vue.svg', import.meta.url).href
+import BoldIcon from './bold-icon.svg'
+import ItalicIcon from './italic-icon.svg'
+import StrikeIcon from './strike-icon.svg'
+import CodeIcon from './code-icon.svg'
+
+// 统一暴露svg文件
+export {
+    BoldIcon,
+    ItalicIcon,
+    StrikeIcon,
+    CodeIcon
+}
