994d880b5fe080c988757ee72db4e1f35d17f6ad
qiuny <Qwer@1230>
2024-06-12 23:33:19 +0800

    feat:采用混合模式配置，灵活发布模块

--- vite.config.js
+++ vite.config.js
@@ -5,31 +5,54 @@ import { resolve } from 'path';
 import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [vue(), svgLoader()],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+    if (mode === 'lib') {
+        return {
+            plugins: [vue(), svgLoader()],
+            resolve: {
+                alias: {
+                    '@': fileURLToPath(new URL('./src', import.meta.url))
+                }
+            },
+            base: '/',
+            build: {
+                outDir: 'dist',
+                lib: {
+                    entry: resolve(__dirname, "./src/components/vue3-major-editor/index.js"),
+                    name: 'Vue3MajorEditor', // 暴露的全局变量
+                    fileName: 'vue3-major-editor' // 输出的包文件名，默认是package.json的name选项
+                },
+                // 库编译模式配置
+                rollupOptions: {
+                    // 确保外部化处理那些你不想打包进库的依赖
+                    external: ["vue"],
+                    output: {
+                        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+                        globals: {
+                            vue: "Vue"
+                        }
+                    }
+                }
+            }
         }
-    },
-    base: '/',
-    build: {
-        outDir: 'dist',
-        lib: {
-            entry: resolve(__dirname, "./src/components/vue3-major-editor/index.js"), 
-            name: 'Vue3MajorEditor', // 暴露的全局变量
-            fileName: 'vue3-major-editor' // 输出的包文件名，默认是package.json的name选项
-        },
-        // 库编译模式配置
-        rollupOptions: {
-            // 确保外部化处理那些你不想打包进库的依赖
-            external: ["vue"],
-            output: {
-                // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-                globals: {
-                    vue: "Vue"
+    } else {
+        // 构建应用
+        return {
+            plugins: [vue(), svgLoader()],
+            resolve: {
+                alias: {
+                    '@': fileURLToPath(new URL('./src', import.meta.url))
+                }
+            },
+            base: '/',
+            build: {
+                outDir: 'app',
+                rollupOptions: {
+                    input: {
+                        main: resolve(__dirname, 'index.html')
+                    }
                 }
             }
         }
     }
-});
+})
