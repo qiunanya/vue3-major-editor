2dd35a2d22a0886c38d74348317e41b022c337c1
qiunanya <1766226354@qq.com>
2024-06-14 13:57:02 +0800

    增加svg图标

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -3,11 +3,31 @@ import BoldIcon from './bold-icon.svg'
 import ItalicIcon from './italic-icon.svg'
 import StrikeIcon from './strike-icon.svg'
 import CodeIcon from './code-icon.svg'
+import BlockquoteIcon from './blockquote-icon.svg'
+import BulletIcon from './bullet-icon.svg'
+import HIcon from './h-icon.svg'
+import HorizontalRuleIcon from './horizontal-rule-icon.svg'
+import OrderedIcon from './ordered-icon.svg'
+import ParagraphIcon from './paragraph-icon.svg'
+import RedoIcon from './redo-icon.svg'
+import UndoIcon from './undo-icon.svg'
+import UnderlineIcon from './underline-icon.svg'
+import ClearIcon from './clear-icon.svg'
 
 // 统一暴露svg文件
 export {
     BoldIcon,
     ItalicIcon,
     StrikeIcon,
-    CodeIcon
+    CodeIcon,
+    BlockquoteIcon,
+    BulletIcon,
+    HIcon,
+    HorizontalRuleIcon,
+    OrderedIcon,
+    ParagraphIcon,
+    RedoIcon,
+    UndoIcon,
+    UnderlineIcon,
+    ClearIcon
 }
