b9f7ddad5f3830b08a6785086e513260e84bad35
qiunanya <1766226354@qq.com>
2024-06-24 15:37:49 +0800

    优化

--- playground/src/test/CustomEditor.ts
+++ playground/src/test/CustomEditor.ts
@@ -1,4 +1,8 @@
-import { defineComponent, h, ref, nextTick, onBeforeUnmount, Teleport } from "vue";
+import { defineComponent, h, ref, nextTick, onBeforeUnmount, PropType, RenderFunction } from "vue";
+interface RenderContext {
+    isEnable: boolean;
+    message: string;
+}
 
 export default defineComponent({
     name: "CustomEditor",
@@ -8,7 +12,7 @@ export default defineComponent({
             default: false
         },
         message: {
-            type: String,
+            type: String as PropType<string>,
             required: true,
         },
     },
@@ -21,15 +25,17 @@ export default defineComponent({
         onBeforeUnmount(() => {
             console.log("Component is about to be unmounted.");
         });
+
+        return { counter, props, context }
     },
-    render:(ctx:any) => {
-        console.log(ctx.message, 444);
+    render:(context:RenderContext) => {
+        console.log(context, 444);
 
         return h('div', {
             class: 'my-editor-as',
             style: {
                 color: 'red'
             },
-        }, [h('div', { class: 'cus-content'}, ctx.message)])
+        }, [h('div', { class: 'cus-content'}, context.message)])
     }
 });
