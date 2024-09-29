2b533a9c99821738a1e55fa6b30fb42ad47511f1
qiunanya <1766226354@qq.com>
2024-09-29 10:48:50 +0800

    chore:output.exports: "named"

--- packages/image-viewer/vite.config.ts
+++ packages/image-viewer/vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
         rollupOptions: {
             external: ["vue"],
             output: {
+                exports: "named",
                 globals: {
                     vue: "Vue",
                 },
