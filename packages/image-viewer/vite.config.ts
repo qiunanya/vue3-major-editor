c108d1d59c8f5fcdec6bb34bbfeabcdb2d71411b
qiunanya <1766226354@qq.com>
2024-10-08 16:58:01 +0800

    chore：增加umd文件打包功能

--- packages/image-viewer/vite.config.ts
+++ packages/image-viewer/vite.config.ts
@@ -5,7 +5,10 @@ import dts from "vite-plugin-dts";
 import pkg from './package.json';
 
 export default defineConfig({
-    plugins: [vue(), dts()],
+    plugins: [vue(), dts({
+        outDir: ['es','lib'],
+        include: ["src/**/*.ts", "src/**/*.tsx"]
+    })],
     server: {
         port: 3000
     },
@@ -13,17 +16,28 @@ export default defineConfig({
         lib: {
             entry: path.resolve(__dirname, "src/index.ts"),
             name: pkg.scope,
-            fileName: 'index',
-            formats:['es','umd','iife']
+            // fileName: 'index',
+            // formats:['es','cjs']
         },
         rollupOptions: {
             external: ["vue"],
-            output: {
-                exports: "named",
-                globals: {
-                    vue: "Vue",
+            output: [
+                {
+                    format: 'es',
+                    dir: 'es',
+                    entryFileNames: '[name].js',
+                    preserveModulesRoot: 'src',
+                    preserveModules: true,
                 },
-            },
+                {
+                    format: 'cjs',
+                    dir: 'lib',
+                    entryFileNames: '[name].js',
+                    preserveModulesRoot: 'src',
+                    preserveModules: true,
+                    exports: 'named'
+                }
+            ]
         },
     },
 });
