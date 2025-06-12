ab66e14552c10775ae87efd204db494941db8566
qiunanya <1766226354@qq.com>
2025-06-12 10:50:02 +0800

    chore:移除状态管理

--- packages/tiptap-editor-vue3/main.ts
+++ packages/tiptap-editor-vue3/main.ts
@@ -1,14 +1,11 @@
 import { createApp } from "vue";
-import { createPinia } from 'pinia';
 
 import App from "./App.vue";
 // 引入组件
 import EditorTiptapVue3 from "./src/index";
 import "./src/style/index.css";
 
-const pinia = createPinia()
 const app = createApp(App)
-app.use(pinia)
 app.use(EditorTiptapVue3)
 document.title = "Vue3图片预览器组件"
 app.mount("#app")
